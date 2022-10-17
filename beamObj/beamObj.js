c90a41f7c2d71ea0439afaffccb8b4eabc47c4c5
kompoj <yord22648@gmail.com>
2022-10-17 15:34:04 +0700

    add 1st injection

--- beamObj/beamObj.js
+++ beamObj/beamObj.js
@@ -38,6 +38,12 @@ function inititialize() {
 }
 
 
+// injection
+document.querySelector("#header").innerHTML = '<nav style="display: flex; justify - content: space - between; ">< div style = "font-size:2.5rem; font-family: caveat; user-select: none;" > SectionCheck</ ><a>about</a></nav >'
+
+
+
+
 
 document.querySelectorAll(".accordion").forEach(El => {
 	El.querySelector(".accordion-header").addEventListener("click", () => {
