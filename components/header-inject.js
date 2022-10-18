d184ce3f5a5071c98a6179c5fd5e1a71fac6d2ec
kompoj <yord22648@gmail.com>
2022-10-18 23:08:31 +0700

    hamburger and closebtn

--- components/header-inject.js
+++ components/header-inject.js
@@ -1,5 +1,10 @@
 ﻿// injection
-document.querySelector("#header-inject").innerHTML = `<nav>
+document.querySelector("#header-inject").innerHTML = `
+<nav>
+	<svg class="hamburger" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+		<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+	</svg>
 	<a id="sectioncheck-logo" href="/">SectionCheck</a>
 	<a href="/about.html">about</a>
-</nav>`
+</nav>
+`
--- components/sidebar-inject.js
+++ components/sidebar-inject.js
@@ -1,3 +1,18 @@
 ﻿// injection
-document.querySelector("#sidebar-inject").innerHTML = `<a href="/public pages/RC beam.html">RC beam</a>
-<a href="/steel wide flange.html">steel wide flange</a>`
+document.querySelector("#sidebar-inject").innerHTML = `
+<svg class="closebtn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+</svg>
+
+<a href="/public pages/RC beam.html">RC beam</a>
+<a href="/steel wide flange.html">steel wide flange</a>
+`
+
+
+document.querySelector(".hamburger").addEventListener("click", () => {
+	document.querySelector("#sidebar-inject").classList.add("open")
+})
+
+document.querySelector(".closebtn").addEventListener("click", () => {
+	document.querySelector("#sidebar-inject").classList.remove("open")
+})
